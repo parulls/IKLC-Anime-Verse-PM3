5511b71c20ae0e0b54f9cc5a5190c1fe9e6c4155
Parulian Dwi Reslia Simbolon <paruliandwireslia22@gmail.com>
2025-12-11 13:38:11 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -36,17 +35,13 @@ android {
         create("release") {
             keyAlias = keystoreProperties["keyAlias"] as? String
             keyPassword = keystoreProperties["keyPassword"] as? String
-            // ensure storeFile is converted to String before calling file(...)
             storeFile = (keystoreProperties["storeFile"] as? String)?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as? String
         }
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.anime_verse"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -55,8 +50,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("release")
         }
     }
